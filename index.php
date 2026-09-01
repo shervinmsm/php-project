e48e8203cb1959f090285db3158a30b589779b4b
shervinmsm <shervinmsm.work@gmail.com>
2026-09-01 19:45:51 +0330

    Add posts from database

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-include "./include/layout/header.php"; ?>
+include "./include/layout/header.php";
+
+$posts = $db->query("SELECT * FROM posts ORDER BY id DESC");
+?>
 
 
 <main>
@@ -12,110 +15,48 @@ include "./include/layout/header.php"; ?>
             <!-- Posts Content -->
             <div class="col-lg-8">
                 <div class="row g-3">
-                    <?php $posts = $db->query("SELECT * FROM posts"); ?>
                     <?php if ($posts->rowCount() > 0): ?>
                         <?php foreach ($posts as $post): ?>
-                            <div class="col-sm-6">
-                                <div class="card">
-                                <img src="./uploads/posts/<?= $post["image"] ?>" class="card-img-top" alt="post-image" />
-                                    <div class="card-body">
-                                        <div class="d-flex justify-content-between">
-                                            <h5 class="card-title fw-bold">
-                                                لورم ایپسوم
-                                            </h5>
-                                            <div>
-                                                <span class="badge text-bg-secondary">طبیعت</span>
-                                            </div>
-                                        </div>
-                                        <p class="card-text text-secondary pt-3">
-                                            لورم ایپسوم متن ساختگی با تولید
-                                            سادگی نامفهوم از صنعت چاپ و با
-                                            استفاده از طراحان گرافیک است.
-                                            چاپگرها و متون بلکه روزنامه و
-                                            مجله در ستون و سطرآنچنان که لازم
-                                            است و برای شرایط فعلی تکنولوژی
-                                            مورد نیاز و کاربردهای متنوع با
-                                            هدف بهبود
-                                        </p>
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <a href="single.html" class="btn btn-sm btn-dark">مشاهده</a>
-
-                                            <p class="fs-7 mb-0">
-                                                نویسنده : علی شیخ
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                            <?php endforeach ?>
+                            <?php
+                            $categoryId = $post['category_id'];
+                            $postCategory = $db->query("SELECT * FROM categories WHERE id = $categoryId")->fetch()
                             ?>
-                        <?php endif ?>
-
-
-                        <div class="card">
-                            <img src="./assets/images/5.jpg" class="card-img-top" alt="post-image" />
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between">
-                                    <h5 class="card-title fw-bold">
-                                        لورم ایپسوم
-                                    </h5>
-                                    <div>
-                                        <span class="badge text-bg-secondary">گردشگری</span>
-                                    </div>
-                                </div>
-                                <p class="card-text text-secondary pt-3">
-                                    لورم ایپسوم متن ساختگی با تولید
-                                    سادگی نامفهوم از صنعت چاپ و با
-                                    استفاده از طراحان گرافیک است.
-                                    چاپگرها و متون بلکه روزنامه و
-                                    مجله در ستون و سطرآنچنان که لازم
-                                    است و برای شرایط فعلی تکنولوژی
-                                    مورد نیاز و کاربردهای متنوع با
-                                    هدف بهبود
-                                </p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <a href="single.html" class="btn btn-sm btn-dark">مشاهده</a>
-
-                                    <p class="fs-7 mb-0">
-                                        نویسنده : علی شیخ
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                            </div>
                             <div class="col-sm-6">
                                 <div class="card">
-                                    <img src="./assets/images/6.jpg" class="card-img-top" alt="post-image" />
+                                    <img src="./uploads/posts/<?= $post['image'] ?>" class="card-img-top" alt="post-image" />
                                     <div class="card-body">
                                         <div class="d-flex justify-content-between">
                                             <h5 class="card-title fw-bold">
-                                                لورم ایپسوم
+                                                <?= $post["title"] ?>
                                             </h5>
                                             <div>
-                                                <span class="badge text-bg-secondary">متفزقه</span>
+                                                <span class="badge text-bg-secondary">
+                                                    <?= $postCategory['title']?>
+                                                </span>
                                             </div>
                                         </div>
                                         <p class="card-text text-secondary pt-3">
-                                            لورم ایپسوم متن ساختگی با تولید
-                                            سادگی نامفهوم از صنعت چاپ و با
-                                            استفاده از طراحان گرافیک است.
-                                            چاپگرها و متون بلکه روزنامه و
-                                            مجله در ستون و سطرآنچنان که لازم
-                                            است و برای شرایط فعلی تکنولوژی
-                                            مورد نیاز و کاربردهای متنوع با
-                                            هدف بهبود
+                                            <?= substr($post["body"], 0, 500) ?>
                                         </p>
                                         <div class="d-flex justify-content-between align-items-center">
                                             <a href="single.html" class="btn btn-sm btn-dark">مشاهده</a>
 
                                             <p class="fs-7 mb-0">
-                                                نویسنده : علی شیخ
+                                                نویسنده :
+                                                <?= $post['author'] ?>
                                             </p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                        <?php endforeach ?>
+                    <?php else: ?>
+                        <div class="col">
+                            <div class="alert alert-danger" مقاله ای یافت نشد ... </div>
+                            </div>
+  
+                    <?php endif ?>
+
                 </div>
             </div>
 
